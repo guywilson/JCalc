b60969fb988abc23dac7e4f2af0c9c2475bbba60
Guy Wilson <guywilson@users.noreply.github.com>
2020-08-30 16:41:40 +0100

    Remove unnecessary C code, we're using a script instead

--- src/c/calc.h
+++
@@ -1,9 +0,0 @@
-#ifndef _INCL_CALC
-#define _INCL_CALC
-
-#define CLASS_PATH		"/Library/Java/Extensions/jcalc-1.0.0.jar:/Library/Java/Extensions/jline-3.11.0.jar:/Library/Java/Extensions/Logger-1.0.jar"
-//#define CLASS_PATH	"./target/jcalc-1.0.0.jar:/Library/Java/Extensions/jline-3.11.0.jar:/Library/Java/Extensions/Logger-1.0.jar"
-
-#define CMD_BUFFER_SIZE	4096
-
-#endif
